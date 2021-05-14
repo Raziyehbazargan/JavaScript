33cf2e9a1bcfc18e70eba6e9c714c89a02e01b42
Raziyeh Bazargan <v-rbazar@microsoft.com>
2021-05-13 21:39:48 -0700

    Closures

---
+++ Interview-Concepts/Closures.js
@@ -0,0 +1,44 @@
+//A closure is a feature in JavaScript where an inner function has access to the outer (enclosing) function’s variables — a scope chain.
+
+//function along with it's lexical scope bundle together forms a closure
+function outer() {
+    var a = 0;
+    function inner() {
+        a++;
+        console.log(a);
+    }
+    return inner;
+}
+
+var y = outer();
+y();  //output: 1
+y();  //output: 2
+y();  //output: 3
+
+
+//print numbers 1 - 5  : because let is block scope it forms a closure and in each iteration callback function get bind with a new copy of i
+function printNumbers() {
+    for(let i = 1; i <= 5; i++) {
+        setTimeout(() => {
+            console.log(i)
+        }, i * 1000);
+    }
+
+    console.log('Hello Javascript!');
+}
+
+//no let
+function printNumbers() {
+    for(var i = 1; i <= 5; i++) {
+        function wrapper(a) {
+            setTimeout(() => {
+                console.log(a)
+            }, a * 1000);
+        }
+
+        wrapper(i);
+    }
+
+    console.log('Hello Javascript!');
+}
+printNumbers();
